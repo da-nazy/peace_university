53b2fe952a2c47859ce5d8fc361ecb72b5d7259d
Ani Daniel Chukwuemeka <danielani660@gmail.com>
2021-03-10 17:05:43 +0100

    second commite

--- src/components/Header/Headerwrapper.js
+++ src/components/Header/Headerwrapper.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './Header.js';
+import '../../style/headerwrapper.css';
 import Customslider from './Customslider.js';
+import  '../../style/header.css';
 import unibackground from '../../images/unibackground.jpg';
 export default function Headerwrapper(){
     return(
         <div className="headerWrapper">
         <Header/>
-    <Customslider/>
+         <Customslider/>
+         <div className="headCover"></div>
         </div>
     )
 }
--- src/components/Header/Right_header.js
+++ src/components/Header/Right_header.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import '../../style/right_header.css';
-import { FaSearch } from "react-icons/fa";
+import { FaSearch ,FaBars} from "react-icons/fa";
 import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../../../node_modules/popper.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {slide as Menu} from 'react-burger-menu';
 export default function Right_header(){
+// for dropmenu
+
+
+    const dropItem=[
+        {
+            name:"SERVICES",
+        },
+        {
+            name:"TEAM",
+        },
+        {
+            name:"GALLERY",
+        }
+    ]
     return(
         <div className="right_header">
-        <ul>
+        <ul className="rtHead">
             <li>HOME</li>
             <li>ABOUT</li>
 
             <li className="dropdown">
             <button className="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown button
+            Dropdown 
             </button>
             <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a className="dropdown-item" href="#">SERVICES</a>
-            <a className="dropdown-item" href="#">TEAM</a>
-            <a className="dropdown-item" href="#">GALLERY</a>
+                {dropItem.map((dropItem,key)=>
+                <a className="dropdown-item" href="#" key={key}>{dropItem.name}</a>
+                )}
+            
             </div>
             </li>
             <li>TESTIMONIAL</li>
@@ -27,6 +43,9 @@ export default function Right_header(){
            </li> 
         <li> <i className="search_but" ><FaSearch/></i></li>
         </ul>
+        <div className="dpdown">
+        <i><FaBars/></i>
+        </div>
         </div>
 
     )
